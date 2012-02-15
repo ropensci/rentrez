8b7b672d482bd165716a0bb8146080137e64fca4
David Winter <winda002@student.otago.ac.nz>
2012-02-15 22:46:28 +1300

    start on documentation

--- R/entrez_fetch.r
+++ R/entrez_fetch.r
@@ -1,12 +1,19 @@
-#' Search the NCBI's entrez database
+#' Download data from NCBI databases
 #'
 #'
 #'
-#'
-#'
-#'
-#'
-#'name
+#'@export
+#'@param db character Name of the database to use
+#'@param ids integer Unique ID(s) to get data from
+#'@param file_format character Format in which to get data (eg, fasta, xml...)
+#'@param \dots character Additional terms to add to the request
+#'@return chracter string containing the file created
+#' @examples
+#' 
+#' snail_search <- entrez_search("nuccore", "Gastropoda[Organism]")
+#' records <- entrez_fetch("nuccore", snail_search$ids[1:2], "fasta")
+
+
 
 entrez_fetch <- function(db, ids, file_format, ...){
     args <- c(db=db, id=paste(ids, collapse=","), rettype=file_format, 
--- R/entrez_search.r
+++ R/entrez_search.r
@@ -1,12 +1,25 @@
-#' Search the NCBI's entrez database
+#' Search the NCBI databases using EUtils
+#'
+#' Contstructs a url  with the given arguments, are downloads xml record
+#' returned by that url. See the package-level documentation for general advice
+#' on using the EUtils functions. 
 #'
 #'
-#'
-#'
-#'g
-#'
-#'
-#'name
+#'@export
+#'@param db character Name of the database to search for
+#'@param term character The search term
+#'@param \dots character Additional terms to add to the request 
+#
+#'@return ids integer Unique IDS returned by the search
+#'@return count integer Total number of hits for the search
+#'@return retmax integer Maximum number of hits returned by the search 
+#'@return file XMLInternalDocument xml file resulting from search, parsed with
+#'\code{\link{XML::xmlTreeParse}}
+#' @examples
+#' 
+#' pubmed_search <- entrez_search(db="pubmed", term="Dwarf Elephant", retmax=1)
+#' other_data <- entrez_link(db="all", ids=pubmed_search$ids, dbfrom="pubmed")
+
 
 entrez_search <- function(db, term, ... ){
     args <- c(db=db, term=term, email=entrez_email, tool=entrez_tool, ...)
