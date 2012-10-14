84199164ac6818ee124ef07c1d6f5f6e8f9a77c5
David Winter <winda002@student.otago.ac.nz>
2012-10-14 19:34:39 +1300

    caught typo in help

--- R/help.r
+++ R/help.r
@@ -6,7 +6,7 @@
 #' Users are expected to know a little bit about the EUtils API, which is well
 #' documented: \url{http://www.ncbi.nlm.nih.gov/books/NBK25500/}
 #'
-#' The NCBI will ban IPs that don't use EUtils withing their \href{http://www.ncbi.nlm.nih.gov/corehtml/query/static/eutils_help.html}{user guidelines}. In particular
+#' The NCBI will ban IPs that don't use EUtils within their \href{http://www.ncbi.nlm.nih.gov/corehtml/query/static/eutils_help.html}{user guidelines}. In particular
 #' /enumerated{
 #'  /item  Don't sen more than three request per second (rentrez enforces this limit)
 #'  /item  If you plan on sending a sequence of more than ~100 requests, do so outside of peak times for the US
