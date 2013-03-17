62314351e760d943ba74dc3378a60102eaaa38e5
David Winter <winda002@student.otago.ac.nz>
2013-03-17 19:24:36 +1300

    esummary parser handles requests with >1 ID

--- R/entrez_summary.r
+++ R/entrez_summary.r
@@ -2,29 +2,33 @@
 #'
 #' Contstructs a query from the given arguments, including a database name and
 #' list of of unique IDs for that database then downloads the XML document 
-#' created by that query. As the XML formats used by various NCBI databases 
-#' differ from each other, no attempt is made to parse the xml file.
-#' See the package-level documentation for general advice
-#' on using the EUtils functions. 
+#' created by that query. The XML document is parsed, with the 
 #'
 #'@export
 #'@param db character Name of the database to search for
-#'@param \dots character Additional terms to add to the request 
-#
+#'@param \dots character Additional terms to add to the request. Requires either
+#'   ID (unique id(s) for records in a given database) or WebEnv (a character
+#'   containing a cookie created by a previous entrez query).
+#'@return A list of esummary records (if multiple IDs are passed) or a single
+#' record.
 #'@return file XMLInternalDocument xml file resulting from search, parsed with
 #'\code{\link{xmlTreeParse}}
 #' @examples
 #'\dontrun{
-#'  popset_summ <- entrez_summary(db="popset", id=07082412)
-#'  popset_summ["
-#'  
+#'  pop_ids = c("307082412", "307075396", "307075338", "307075274")
+#'  pop_summ <- entrez_summary(db="popset", id=pop_ids)
+#'  sapply(popset_summ, "[[", "Title")
 #'}
 
 entrez_summary <- function(db, ...){
     url_string <- make_entrez_query("esummary", db=db,
                                     require_one_of=c("id", "WebEnv"), ...)
-    record <- xmlTreeParse(getURL(url_string), useInternalNodes=TRUE)
-    return(lapply(record["//DocSum"], parse_esummary))
+    whole_record <- xmlTreeParse(getURL(url_string), useInternalNodes=TRUE)
+    rec <- lapply(whole_record["//DocSum"], parse_esummary)
+    if(length(rec) == 1){
+        return(rec[[1]])
+    }
+    return(rec)
 }
 
 #' @S3method print esummary
@@ -68,7 +72,7 @@ parse_node <- function(node) {
 parse_esumm_int <- function(node) as.integer(xmlValue(node))
 
 parse_esumm_list <- function(node){
-    res <- lapply(node["Item"], parse_summary)
+    res <- lapply(node["Item"], parse_node)
     names(res) <- lapply(node["Item"], xmlGetAttr, "Name")
     return(res)
 }
--- inst/tests/test_fetch.r
+++ inst/tests/test_fetch.r
@@ -2,6 +2,8 @@ test_that("Functions to fetch records & summaries work", {
           #setup
           pop_ids = c("307082412", "307075396", "307075338", "307075274")
           pop_summ <- entrez_summary(db="popset", id=pop_ids)
+          sts_summ <- entrez_summary(db="unists", id =33703)
+          summ_list_ele <- sts_summ[["Map_Gene_Summary_List"]]
           coi <- entrez_fetch(db = "popset", id = pop_ids[1], 
                               rettype = "fasta")
 
@@ -10,6 +12,11 @@ test_that("Functions to fetch records & summaries work", {
           expect_that(length(pop_summ), equals(4))
           sapply(pop_summ, function(x)
                  expect_that(x[["Title"]], matches("Muraenidae")))
+          #do lists and structures work?
+          expect_that(summ_list_ele, is_a("list"))
+          expect_true(length(summ_list_ele) > 0)
+          expect_true(length(summ_list_ele[[1]]) >0 )
+          #does fetching sequence records work?
           expect_that(length(strsplit(coi, ">")[[1]]), 
                          equals(30))
 })
