83ef4e70f65e009ccdfe9d75fe463dc643fa6526
David Winter <david.winter@gmail.com>
2014-09-21 21:38:07 -0700

    more tests for httr GETs

--- tests/testthat/test_query.r
+++ tests/testthat/test_query.r
@@ -1,10 +1,15 @@
 context("query")
 test_that("Query building functions work", {
 
-    #concatenate multiple IDs, include entrez terms
-    query <- rentrez:::make_entrez_query("test", id=c(123,321))
-    expect_that(query, matches("&id=123,321"))
-    expect_that(query, matches("email=david.winter@gmail.com&tool=rentrez"))
+    #concatenate multiple IDs, include entrez terms 
+    query <- rentrez:::make_entrez_query("efetch", 
+                                         db="nuccore", 
+                                         id=c(443610374, 443610372),
+                                         retmode="txt",
+                                         rettype="fasta")
+    nrecs <- length(gregexpr(">", query)[[1]])
+   
+    expect_equal(nrecs, 2)
     
     #"require one of" functions work
     expect_that(rentrez:::make_entrez_query("test", 
@@ -13,13 +18,29 @@ test_that("Query building functions work", {
                 throws_error())
 
     #should be able to give ints or characters to id and get a url
-    q_char <- rentrez:::make_entrez_query("test", id=c("123", "321"))
-    expect_that(query, matches("&id=123,321"))
+    query <- rentrez:::make_entrez_query("efetch", 
+                                         db="nuccore", 
+                                         id=c("443610374", "443610372"),
+                                         retmode="txt",
+                                         rettype="fasta")
+    nrecs <- length(gregexpr(">", query)[[1]])
+    expect_equal(nrecs, 2)
 
     #specific function have right "require one of" settings
     expect_that(entrez_fetch(db="nuccore", rettype="fasta"), throws_error())
     expect_that(entrez_summary(db="nuccore"), throws_error())
     expect_that(entrez_link(db="nuccore", dbfrom="pubmed"), throws_error())
+
+    #httr pases on errors
+    #404
+    expect_error(rentrez:::make_entrez_query("non-eutil", 
+                                             id=12, 
+                                             db="none"))
+    #400
+    expect_error(rentrez:::make_entrez_query("efetch", 
+                                             id=1e-17,
+                                             db="nuccore"))
+    
 })
 
 
