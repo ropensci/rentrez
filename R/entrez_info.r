770116c3c7fd0a8d073ee4eae5178a39615d5912
David Winter <david.winter@gmail.com>
2014-10-04 20:06:00 -0700

    added docs for entrez_info

--- R/entrez_info.r
+++ R/entrez_info.r
@@ -1,19 +1,19 @@
 #' Get infromation about EUtils databases
 #'
-#' Contstructs a query to NCBI's einfo and return parsed XML object
-#' Note The most common uses for einfo API are to list fields against which
-#' a database can be searched, or linked databases avaliabl,
-#' @param db characater database about which to retreive information (optional,
-#' @param config config vector passed on to \code{httr::GET}
-#' @return XMLInternalDocument with information describing either all the
-#' databases avaliable in Eutils (if db is not set) or one pariticular database
-#' (set by 'db')
-#' @seealso \code{\link[httr]{config}} for avaliable httr configurations
-#' @family einfo
-#' @examples
-#' all_the_data <- entrez_info()
-#' xpathSApply(all_the_data, "//DbName", xmlValue)
-#' @export
+#'Contstructs a query to NCBI's einfo and return parsed XML object
+#'Note The most common uses for einfo API are to list fields against which
+#'a database can be searched, or linked databases avaliabl,
+#'@param db characater database about which to retreive information (optional,
+#'@param config config vector passed on to \code{httr::GET}
+#'@return XMLInternalDocument with information describing either all the
+#'databases avaliable in Eutils (if db is not set) or one pariticular database
+#'(set by 'db')
+#'@seealso \code{\link[httr]{config}} for avaliable httr configurations
+#'@family einfo
+#'@examples
+#'all_the_data <- entrez_info()
+#'xpathSApply(all_the_data, "//DbName", xmlValue)
+#'@export
 
 entrez_info <- function(db=NULL, config=NULL){
     response <- make_entrez_query("einfo", db=db, config=config)
