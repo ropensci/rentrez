975e4838d395a679e3028263164a94b8bec6004f
David Winter <david.winter@gmail.com>
2014-10-05 16:49:23 -0700

    elink docs have einfo examples integrated

--- R/entrez_link.r
+++ R/entrez_link.r
@@ -1,11 +1,13 @@
 #' Get links to datasets related to a unique ID from an NCBI database
 #'
 #' Contstructs a query with the given arguments and downloands the XML
-#' document created by that query. Unique IDs from linked-databases
+#' document created by that query. 
 #'
 #'@import XML
 #'@export
-#'@param db character Name of the database to search for links (or use "all" to earch all NCBI databases
+#'@param db character Name of the database to search for links (or use "all" to 
+#' search all dabases avaliable for \code{db}. \code{entrez_db_links} allows you
+#' to discover datbases that might have linked information (see examples).
 #'@param dbfrom character Name of database from which the Id(s) orginate
 #'@param \dots character Additional terms to add to the request
 #'@param config vector configuration options passed to httr::GET  
@@ -15,10 +17,13 @@
 #'@return file XMLInternalDocument xml file resulting from search, parsed with
 #'\code{\link{xmlTreeParse}}
 #' @examples
-#'
-#'  pubmed_search <- entrez_search(db = "pubmed", term = "10.1016/j.ympev.2010.07.013[doi]")
-#'  linked_data <- entrez_link(dbfrom = "pubmed", id = pubmed_search$ids, db = "all")
-#' nucleotide_IDs <- linked_data$pubmed_nuccore
+#' \donttest{
+#'  (pubmed_search <- entrez_search(db = "pubmed", term ="10.1016/j.ympev.2010.07.013[doi]"))
+#'  (linked_dbs <- entrez_db_links("pubmed"))
+#'  nucleotide_data <- entrez_link(dbfrom = "pubmed", id = pubmed_search$ids, db ="nuccore")
+#'  #All the links
+#'  entrez_link(dbfrom="pubmed", db="all", id=pubmed_search$ids)
+#'}
 #'
 
 entrez_link <- function(db, dbfrom, config=NULL, ...){
