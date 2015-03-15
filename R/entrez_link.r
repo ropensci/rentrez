6cd462d02504abb154c473c1ff6f24035ea3315f
David Winter <david.winter@gmail.com>
2015-03-14 21:30:54 -0700

    Start on #39
    New design follows a similar pattern as entrez_summary's internal
    parsing functions:
    
    * There are specific parsers for each possible elink XML (neighbour, and
       neighbour history for now)
    * These methods are dispatched via a higher fxn which is bassically a
      switch statement
    
    To handle printing of the new records, each object has an attribute
    called 'content' that briefly describes what the object contains.

--- R/entrez_link.r
+++ R/entrez_link.r
@@ -9,6 +9,7 @@
 #' search all databases available for \code{db}. \code{entrez_db_links} allows you
 #' to discover databases that might have linked information (see examples).
 #'@param dbfrom character Name of database from which the Id(s) originate
+#'@param cmd link function to use
 #'@param \dots character Additional terms to add to the request
 #'@param config vector configuration options passed to httr::GET  
 #'@seealso \code{\link[httr]{config}} for available configs 
@@ -27,39 +28,76 @@
 #'
 
 
-entrez_link <- function(db, dbfrom, config=NULL, ...){
-    response <- make_entrez_query("elink", db=db, dbfrom=dbfrom,
+entrez_link <- function(db, dbfrom, cmd='neighbor', config=NULL, ...){
+
+    response <- make_entrez_query("elink", db=db, dbfrom=dbfrom, cmd=cmd,
                                   config=config, ..., 
                                   require_one_of=c("id", "WebEnv"))
-    record <- XML::xmlTreeParse(response, useInternalNodes=TRUE)
+    record <- parse_response(response, 'xml')
     search_ids <- XML::xpathSApply(record, "//IdList/Id", 
                                    function(x) as.numeric(XML::xmlValue(x))
     )
-    if(-1 %in% search_ids){
-       msg <- paste("Some  IDs not found in", dbfrom)
-       warning(msg)
-    }
-    db_names <- XML::xpathSApply(record, "//LinkName", XML::xmlValue)
-
-    # Get ID from each database
-    # Not simplified so if a single database get a named list (for consistancy)
-    result <- sapply(db_names, get_linked_ids, record=record, simplify=FALSE)
-    result$file <- record
-    class(result) <- c("elink", class(result))
-    #NCBI limits requests to three per second
     Sys.sleep(0.33)
-    return(result)
+    parse_elink(record, cmd=cmd)
 }
+
+#    if(-1 %in% search_ids){
+#       msg <- paste("Some  IDs not found in", dbfrom)
+#       warning(msg)
+#    }
+#    db_names <- XML::xpathSApply(record, "//LinkName", XML::xmlValue)
+#
+#    # Get ID from each database
+#    # Not simplified so if a single database get a named list (for consistancy)
+#    result <- sapply(db_names, get_linked_ids, record=record, simplify=FALSE)
+#    result$file <- record
+#    class(result) <- c("elink", class(result))
+#    #NCBI limits requests to three per second
+#    Sys.sleep(0.33)
+#    return(result)
+#}
 
 #' @export
 
+parse_elink <- function(x, cmd){
+    switch(cmd,
+           "neighbor"         = parse_neighbors(x),
+           "neighbor_score"   = parse_neighbors(x),
+           "neighbor_history" = parse_history(x),
+           x)
+}
+
+parse_neighbors <- function(x){
+    db_names <- XML::xpathSApply(x, "//LinkName", XML::xmlValue)
+    result <- sapply(db_names, get_linked_elements, record=x, element="Id", simplify=FALSE)
+    attr(result, "scores") <- sapply(db_names, get_linked_elements, record=x, element="Score", simplify=FALSE)
+    attr(result, "content") <- "linked IDs"
+    class(result) <- c("elink", class(result))
+    result
+}
+
+parse_history <- function(x){
+    qks <-    XML::xpathSApply(x, "//LinkSetDbHistory/QueryKey", XML::xmlValue)
+    names(qks) <-    XML::xpathApply(x, "//LinkSetDbHistory/LinkName", XML::xmlValue)
+    cookie <- XML::xmlValue(x[["//WebEnv"]])
+    res <- list(cookie = cookie, QueryKeys=qks)
+    attr(res, "content") <- "web history information"
+    res
+}
+    
+
+parse_linkouts <- function(x){
+}
+
+
+#' @export
 print.elink <- function(x, ...){
    len <- length(x)
    cat(paste("elink result with ids from", len - 1, "databases:\n"))
    print (names(x)[-len], quote=FALSE)
 }
 
-get_linked_ids <- function(record, dbname){
-    path <-  paste("//LinkSetDb/LinkName[text()='", dbname, "']/../Link/Id", sep="")
+get_linked_elements <- function(record, dbname, element){
+    path <-  paste0("//LinkSetDb/LinkName[text()='", dbname, "']/../Link/", element)
     return(XML::xpathSApply(record, path, XML::xmlValue))
 }
