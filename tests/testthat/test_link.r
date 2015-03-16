417032e8eb1dda542d84ff829dd9d37dde8cbc53
David Winter <david.winter@gmail.com>
2015-03-15 21:24:50 -0700

    Moar #39, now have tests for every command

--- tests/testthat/test_link.r
+++ tests/testthat/test_link.r
@@ -1,15 +1,26 @@
 context("elink")
-elinks <- entrez_link(dbfrom = "pubmed", id = 20674752, db = "all")
+
+elinks <- entrez_link(dbfrom = "pubmed", id = 19880848, db = "all")
+message("entrez_link tests take some time, as they have to download many recs...")
+commands <- c("neighbor_history", "neighbor_score", 
+              "acheck", "ncheck", "lcheck", 
+              "llinks", "llinkslib", "prlinks")
+
+
+all_the_commands <- lapply(commands, function(cmd_arg)
+    entrez_link(db="pubmed", dbfrom="pubmed", id=19880848, cmd=cmd_arg)
+)
 
 test_that("The record-linking funcitons work",{
     expect_that(elinks, is_a("elink"))
-    expect_that(names(elinks), is_a("character"))
-    expect_that(elinks$file, is_a("XMLInternalDocument"))
+    expect_that(names(elinks$links), is_a("character"))
     expect_true(length(elinks$pubmed_nuccore) > 0)
 })
 
 test_that("Print elink behaves", {
-    expect_output(elinks, "elink result with ids from \\d+")
+    for(ret in all_the_commands){
+        expect_output(elinks, "elink object with contents:\\s+\\$[A-Za-z]+")
+    }
 })
 
 test_that("We detect missing ids from elink results",{
