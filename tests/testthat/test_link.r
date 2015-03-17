3c2ceab958630706a822a16c66405601730b8603
David Winter <david.winter@gmail.com>
2015-03-16 19:28:26 -0700

    test for internal elements of elink recs/ warning on bad cmd arg

--- tests/testthat/test_link.r
+++ tests/testthat/test_link.r
@@ -15,7 +15,6 @@ commands <- c("neighbor_history", "neighbor_score",
 all_the_commands <- lapply(commands, function(cmd_arg)
     entrez_link(db="pubmed", dbfrom="pubmed", id=19880848, cmd=cmd_arg)
 )
-
 test_that("The record-linking funcitons work",{
     expect_that(elinks, is_a("elink"))
     expect_that(names(elinks$links), is_a("character"))
@@ -33,3 +32,19 @@ test_that("We detect missing ids from elink results",{
     entrez_link(dbfrom="pubmed", db="all", id=c(20203609,2020360999999,20203610))
    )
 })
+
+test_that("Elink sub-elements can be acessed and printed", {
+    expect_output(all_the_commands[[3]][[1]], 
+                  "elink result with information from \\d+ databases")
+    expect_output(all_the_commands[[8]]$linkouts[[1]],
+                  "Linkout from [A-Za-z]+\\s+\\$Url")
+})
+
+
+test_that("Elink warns on mis-spelled/unknown cmds",{
+    expect_warning(rcheck <- entrez_link(dbfrom = "pubmed",
+                                         id = 19880848, db = "all", 
+                                         cmd='rcheck'), 
+                   "Command rcheck is not supported")
+})
+                   
