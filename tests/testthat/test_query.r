bbcc9f42c37f43547f82c0d70615eb5636c47925
David Winter <david.winter@gmail.com>
2015-07-11 20:19:25 -0700

    Better tests for checking id/webenv fxns work

--- tests/testthat/test_query.r
+++ tests/testthat/test_query.r
@@ -12,12 +12,6 @@ test_that("Query building functions work", {
    
     expect_equal(nrecs, 2)
     
-    #"require one of" functions work
-    expect_that(rentrez:::make_entrez_query("test", 
-                                            x=10, 
-                                            config=list(),
-                                            require_one_of=c("id", "db")), 
-                throws_error())
 
     #should be able to give ints or characters to id and get a url
     query <- rentrez:::make_entrez_query("efetch", 
@@ -30,8 +24,8 @@ test_that("Query building functions work", {
     expect_equal(nrecs, 2)
 
     #specific function have right "require one of" settings
-    expect_that(entrez_fetch(db="nuccore", rettype="fasta"), throws_error())
-    expect_that(entrez_summary(db="nuccore"), throws_error())
+    expect_that(entrez_fetch(db="nuccore", rettype="fasta", WebEnv="SDA"), throws_error())
+    expect_that(entrez_summary(db="nuccore", query_key=1, WebEnv="ASD", id=123), throws_error())
     expect_that(entrez_link(db="nuccore", dbfrom="pubmed"), throws_error())
 
     #httr pases on errors
