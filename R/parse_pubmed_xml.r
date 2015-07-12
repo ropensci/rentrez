3fd660fc9b66ba8bf95fcb81abd00f26e5ada89b
David Winter <david.winter@gmail.com>
2015-07-12 13:44:59 -0700

    re-wrote docs, parse_pubmed updated to new elink object

--- R/parse_pubmed_xml.r
+++ R/parse_pubmed_xml.r
@@ -11,7 +11,7 @@
 #' hox_paper <- entrez_search(db="pubmed", term="10.1038/nature08789[doi]")
 #' hox_rel <- entrez_link(db="pubmed", dbfrom="pubmed", id=hox_paper$ids)
 #' recs <- entrez_fetch(db="pubmed", 
-#'                        id=hox_rel$pubmed_pubmed[1:3], 
+#'                        id=hox_rel$links$pubmed_pubmed[1:3], 
 #'                        rettype="xml")
 #' parse_pubmed_xml(recs)
 #'
