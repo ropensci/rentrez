7c7ce6b471c6b2aa711f424391cbcb7eb0594dcc
David Winter <david.winter@gmail.com>
2015-07-19 11:35:17 -0700

    tests for by_id mode #51

--- tests/testthat/test_link.r
+++ tests/testthat/test_link.r
@@ -1,6 +1,8 @@
 context("elink")
 
-elinks <- entrez_link(dbfrom = "pubmed", id = 19880848, db = "all")
+elinks_mixed <- entrez_link(dbfrom = "pubmed", id = c(19880848, 22883857), db = "all")
+elinks_by_id <- entrez_link(dbfrom = "pubmed", id = c(19880848, 22883857), db = "all", by_id=TRUE)
+
 
 #
 #We should maybe download these xmls and test the internal functions
@@ -16,14 +18,22 @@ all_the_commands <- lapply(commands, function(cmd_arg)
     entrez_link(db="pubmed", dbfrom="pubmed", id=19880848, cmd=cmd_arg)
 )
 test_that("The record-linking funcitons work",{
-    expect_that(elinks, is_a("elink"))
-    expect_that(names(elinks$links), is_a("character"))
-    expect_true(length(elinks$links$pubmed_mesh_major) > 0)
+    expect_that(elinks_mixed, is_a("elink"))
+    expect_that(names(elinks_mixed$links), is_a("character"))
+    expect_true(length(elinks_mixed$links$pubmed_mesh_major) > 0)
 })
 
-test_that("Print elink behaves", {
+
+test_that("by_id mode works for elinks", {
+    expect_that(elinks_by_id, is_a("elink_list"))
+    expect_that(length(elinks_by_id), equals(2))
+    expect_that(elinks_by_id[[1]], is_a("elink"))
+})
+
+test_that("elink printing behaves", {
+    expect_output(elinks_by_id, "List of 2 elink objects,each containing")
     for(ret in all_the_commands){
-        expect_output(elinks, "elink object with contents:\\s+\\$[A-Za-z]+")
+        expect_output(ret, "elink object with contents:\\s+\\$[A-Za-z]+")
     }
 })
 
@@ -47,4 +57,6 @@ test_that("Elink warns on mis-spelled/unknown cmds",{
                                          cmd='rcheck'), 
                    "Command rcheck is not supported")
 })
-                   
+
+
+
