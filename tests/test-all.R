61e19db7e476e881dd88e35dc4e916512b48d6ec
David Winter <david.winter@gmail.com>
2015-09-12 17:39:43 -0700

    Skip all tests on CRAN.
    Rather than use skip_on_cran() for every single test, we perform
    the same check that this function does (for the devtools set system
    variable 'NOT_CRAN=true') and skip tests entirely if we are on CRAN
    
    Closes #53

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,2 +1,10 @@
 library("testthat")
-test_check("rentrez")
+
+#All of the tests rely on the API existing and behaving as documented. However,
+#the API occasionally falls over or stops working which lets to errors on CRAN.
+#Because we use travis CI we will hear about any test failures as soon as they
+#happen. So, let's skill all tests on CRAN:
+
+if(identical(Sys.getenv("NOT_CRAN"), "true")){
+    test_check("rentrez")
+}
