8ddf0f4a48fe46824c67fe0d9263f01022379d9a
Samuel Bharti <samuelbharti.io@gmail.com>
2026-07-17 05:49:22 -0500

    Fix stray characters in two error messages and an .Rbuildignore pattern
    * entrez_summary(): drop the stray ")" from the "no DocSums" error
    * parse_pubmed_xml(): remove a double space from the unknown-type warning
    * .Rbuildignore: "^revdep$/" never matched anything, use "^revdep/"
    
    Text-only; no change to package behaviour.

--- R/parse_pubmed_xml.r
+++ R/parse_pubmed_xml.r
@@ -38,7 +38,7 @@ parse_one_pubmed <- function(paper){
     if( atype != "PubmedArticle" ){
         pmid = xpathSApply(paper, "//PMID", xmlValue)
         msg = paste0("Pubmed record ", pmid, " is of type '", atype, 
-                     "' which  rentrez doesn't know how to parse.",
+                     "' which rentrez doesn't know how to parse.",
                      " Returning empty record")
 
         warning(msg)
